5c99d046ee0cd3da69170c9032f8364ab1ad779b
Ahmed Omeiza <omeizaahmed81@gmail.com>
2026-02-11 21:40:51 +0100

    first commit

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 import { Suspense } from "react";
 
 async function UserDetails() {
@@ -35,8 +34,6 @@ export default function ProtectedPage() {
         </pre>
       </div>
       <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
